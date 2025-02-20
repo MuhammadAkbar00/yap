8b05dbb7599a7982898945526072933a7b6d0717
MuhammadAkbar00 <fauzanmu2000@gmail.com>
2025-02-20 19:11:21 -0400

    front end init

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import MessageContainer from "../../components/messages/MessageContainer";
+
+const Home = () => {
+  return (
+    <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/20 backdrop-blur-2xl border-2 border-white/30 shadow-xl">
+      <Sidebar />
+      <div className="flex-1 overflow-auto">
+        <MessageContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
