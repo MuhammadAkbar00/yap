3ec51191de8fc5626cb7d0e1d79114d005c628a1
MuhammadAkbar00 <fauzanmu2000@gmail.com>
2025-02-21 00:50:03 -0400

    fixed up socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://yap-0qcr.onrender.com", {
         query: {
           userId: authUser._id,
         },
